49df872e8024bc07de0e7d620a4f4cce17df843d
W <w@WdeMac-mini.lan>
2023-10-11 02:26:12 -0400

    first commit

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <h1 className={styles.title}>Let&apos;s Keep in Touch</h1>
       <div className={styles.content}>
         <div className={styles.imgContainer}>
           <Image
@@ -30,7 +30,7 @@ const Contact = () => {
             cols="30"
             rows="10"
           ></textarea>
-          <Button url="#" text="Send"/>
+          <Button url="#" text="Send" />
         </form>
       </div>
     </div>
